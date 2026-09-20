6026f1ebf36a3b701d2a0627953f1405758eb105
Saleh <salehgmail91@gmail.com>
2026-09-20 07:14:43 -0700

    feat: add Eitaa contact for Pro purchases

--- js/pro-config.js
+++ js/pro-config.js
@@ -8,7 +8,7 @@ window.PRO_CONFIG = {
   PUBLIC_KEY: 'MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEPXMO3oLtr6JgQWNKf2wWezfAMeipEC2iWQAOdJ8CcDsjqFpcKry3yw7m44KE4aK1Eybum1nsgdbWC4-5ZwSDag',
 
   // تنظیمات
-  VERSION: '1.2.1',
+  VERSION: '1.2.2',
   LICENSE_PREFIX: 'MTK1',
   STORAGE_KEY: 'mt-license-v2',
   DEVICE_ID_KEY: 'mt-device-id',
